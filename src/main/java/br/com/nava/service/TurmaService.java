fde6c157476be6bd7e575c2c505f1fc7bc723e26
RafaelaSSantos <rafaela.santos@nava.com.br>
2022-04-13 17:01:36 -0300

    Paginação da aplicação

--- src/main/java/br/com/nava/service/TurmaService.java
+++ src/main/java/br/com/nava/service/TurmaService.java
@@ -3,6 +3,10 @@ package br.com.nava.service;
 import java.util.*;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import br.com.nava.entity.Turma;
 import br.com.nava.repository.TurmaRepository;
@@ -16,6 +20,12 @@ public class TurmaService {
 	public List<Turma> listaTodasTurmas(){
 		return turmaRepository.findAll();
 	}	
+	
+	//Paginação
+	public Page<Turma> buscarPorPaginacao(int pagina, int linhasPorPagina, String direction, String orderBy){
+		PageRequest pageResquest = PageRequest.of(pagina, linhasPorPagina, Direction.valueOf(direction), orderBy);
+		return new PageImpl<>(turmaRepository.findAll(), pageResquest, linhasPorPagina);
+	}
 	
 	public Turma buscaPorID(Integer id) throws ObjectNotFoundException{
 		Optional<Turma> optional = turmaRepository.findById(id);
